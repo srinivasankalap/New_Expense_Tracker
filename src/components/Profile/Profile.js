f3dc68f79087439ab830c308988adab6b68be898
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-29 20:03:33 +0530

    Retreive Updated Data

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,37 @@
 import './Profile.css';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 const Profile=()=>{
     const nameinputRef=useRef();
     const urlInputRef=useRef();
+    const [initialData, setInitialData] = useState({ name: '', profilePhotoUrl: '' });
+
+    useEffect(() => {
+        const id = localStorage.getItem('token');
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBycSmTAkk_MbjTNxVPNXryNlABlqht8Co', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            idToken: id,
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('Failed to fetch data from the server.');
+            }
+          })
+          .then((data) => {
+            console.log(data.users[0].displayName);
+            setInitialData({ name: data.users[0].displayName, profilePhotoUrl: data.users[0].photoUrl });
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      }, []);
+
     const submitHandler=async(e)=>{
         e.preventDefault();
         const id=localStorage.getItem('token')
@@ -35,9 +64,9 @@ const Profile=()=>{
         <form onSubmit={submitHandler}>
             <h1>Contact Details</h1>
             <label htmlFor="name">Name</label>
-            <input id="name" type="name" ref={nameinputRef}/>
+            <input id="name" type="name" ref={nameinputRef} defaultValue={initialData.name}/>
             <label htmlFor="url">Profile Photo URL</label>
-            <input id="url" type="url" ref={urlInputRef}/>
+            <input id="url" type="url" ref={urlInputRef} defaultValue={initialData.profilePhotoUrl}/>
             <button>Update</button>
         </form>
         </>
